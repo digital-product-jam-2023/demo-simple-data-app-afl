0c509515b1693914ddaa83f4d18125813f2dee7d
Paul Walsh <paul@walsh.co.il>
2022-10-18 13:36:03 +0300

    Basic API integration and data render

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-contactButton = document.querySelector("#contact button");
+const API_BASE = 'https://api.squiggle.com.au';
+const API_ENDPOINT = `${API_BASE}/?q=teams`;
+const IMAGE_BASE = 'https://squiggle.com.au';
 
-contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
-});
+const app = document.getElementById('app');
+
+async function getData() {
+  let data = null;
+  try {
+    const response = await fetch(API_ENDPOINT);
+    console.log(response);
+    if (response.status === 200) {
+      data = await response.json();
+    }
+  } catch (error) {
+    console.log('api error');
+    console.error(error);
+  }
+  return data;
+}
+
+async function render() {
+  data = await getData();
+  const { teams } = data;
+  for (const team of teams) {
+
+    console.log(team);
+
+    const containerElement = document.createElement('div');
+    const nameElement = document.createElement('h2');
+    const logoElement = document.createElement('img');
+
+    nameElement.innerHTML = team.name;
+    logoElement.src = `${IMAGE_BASE}${team.logo}`;
+
+    containerElement.appendChild(nameElement);
+    containerElement.appendChild(logoElement);
+
+    app.appendChild(containerElement);
+  }
+}
